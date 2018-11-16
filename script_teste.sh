4917fa4f22a67aa73edad1e5bfb5ce1e39909835
Paulo201898 <42985681+Paulo201898@users.noreply.github.com>
2018-11-16 18:52:48 -0200

    Update script_teste.sh

--- script_teste.sh
+++ script_teste.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Script of Downloads / Progressive count
-echo html_content_$hoje.txt <> alias `echo "Arquivos: $(ls -la |grep "^-"|wc -l)"`
+echo html_content_$$date.txt <> count "+1" | tr -d '\r'
 date;
-echo $hoje=$((hoje+1))
+echo $$date | while count='+1' read -r line
+do
+  ls -l "$line"
+  done
